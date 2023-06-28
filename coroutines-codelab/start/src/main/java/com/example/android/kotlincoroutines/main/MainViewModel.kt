af624f0372149ce885ba14b160793126d831de1e
Wyr' Villazorda <wyrlvillazorda@gmail.com>
2023-06-28 23:19:25 +0800

    initial

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -19,8 +19,11 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.BACKGROUND
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -101,10 +104,9 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Wait one second then update the tap count.
      */
     private fun updateTaps() {
-        // TODO: Convert updateTaps to use coroutines
-        tapCount++
-        BACKGROUND.submit {
-            Thread.sleep(1_000)
+        viewModelScope.launch {
+            tapCount++
+            delay(1_1000)
             _taps.postValue("${tapCount} taps")
         }
     }
@@ -120,7 +122,18 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Refresh the title, showing a loading spinner while it refreshes and errors via snackbar.
      */
     fun refreshTitle() {
-        // TODO: Convert refreshTitle to use coroutines
+        viewModelScope.launch {
+            try{
+                _spinner.value = true
+                repository.refreshTitle()
+            }catch (error: TitleRefreshError){
+                _snackBar.value = error.message
+            } finally {
+                _spinner.value = false
+            }
+        }
+
+
         _spinner.value = true
         repository.refreshTitleWithCallbacks(object : TitleRefreshCallback {
             override fun onCompleted() {
@@ -132,5 +145,7 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
                 _spinner.postValue(false)
             }
         })
+
+
     }
 }
